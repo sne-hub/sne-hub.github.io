2e615a37f790fdf5dff745fa5624251f775d2215
Sinenhlanhla Magubane <snemagubane06@gmail.com>
2022-02-23 11:07:05 +0200

    making the nav bar sticky

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+window.onscroll = function() {myFunction()};
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
