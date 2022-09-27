7dc1dd786c325e123cd92223a7d4d167b3ef7bb8
Sinenhlanhla Magubane <snemagubane06@gmail.com>
2022-09-27 09:55:32 +0200

    adding more functiuonality

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+
 function myFunction() {
-    const element = document.getElementById("topNav");
-    if (element.className === "topnav") {
-        element.className += " responsive";
-    } else {
-        element.className = "topnav";
-    }
+  const element = document.getElementById("topNav");
+  if (element.className === "topnav") {
+    element.className += " responsive";
+  } else {
+    element.className = "topnav";
+  }
 }
-const links = document.getElementsByTagName("a")
-for(let i = 1; i< links.length ; i++){
-    links[i].addEventListener("click", function(){
-        myFunction()
-    })
+const links = document.getElementsByTagName("a");
+for (let i = 1; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    myFunction();
+  });
 }
+const container = document.querySelector(".container");
+const header = document.querySelector(".header");
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  container.style.display = "block";
+  header.style.display = "none";
+  const buttons= document.querySelectorAll(".button")
+  buttons[1].addEventListener("click", () => {
+    container.style.display = "none";
+    header.style.display = "block";
+  });
+});
+
