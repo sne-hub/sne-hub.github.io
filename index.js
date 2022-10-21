1e8401b6cf5bda2f4e1c12bb966e9db019f71a28
Sinenhlanhla Magubane <snemagubane06@gmail.com>
2022-10-21 20:27:54 +0200

    adding a button

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 const home = document.querySelectorAll(".home")
-const otherSpans= Array(document.querySelectorAll(".container")[0].children)
+const button = document.getElementById("button")
+const otherSpans= document.querySelectorAll(".container")
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+    otherSpans[0].style.display="block"
+
 })
 document.querySelectorAll(".nav-link").forEach(element=>{
     element.addEventListener("click", ()=>{
         hamburger.classList.remove("active")
         navMenu.classList.remove("active")
+        otherSpans[0].style.display="block"
+
     } )
+})
+button.addEventListener("click", ()=>{
+    otherSpans[0].style.display="block"
 })
